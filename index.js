fa81d3ce47c45bb0f64faa51ebfb94e5a5bdb696
Myskae <xuelorie@gmail.com>
2021-11-30 23:56:49 +0100

    :)

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ io.on('connection', (socket) => {
 io.on('connection', (socket) => {
 
     socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
+        console.log('message: ' + msg["msg"]);
     });
 
     socket.on('new-user', (name) => {
