094ee885f5ace822f72f59ab30a6174d475acaea
Myskae <xuelorie@gmail.com>
2021-11-30 15:32:33 +0100

    :)

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
-});
+server.listen(process.env.PORT || 5000);
 
 
 io.on('connection', (socket) => {
