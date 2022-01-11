9105f98596ab99654c27a4fec67538248ae58019
Ebium <91456594+Ebium@users.noreply.github.com>
2022-01-11 21:05:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -20,11 +20,24 @@ const con = mysql.createPool({
     host: "eu-cdbr-west-02.cleardb.net",
     user: "b6c855de08ba54",
     password: "5e32ffd5",
-    database: `heroku_f664f164cab04d3`
+    database: `heroku_f664f164cab04d3`,
+    multipleStatements: true
 });
 
+var sql = "DELETE FROM testonline ";
+con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("testonline rafraichi");
+});
 
-
+/* Raffraichi la table ban chaque minute : toutes les 60 000 ms pour enlever les personnes banni qui ont dépasser leurs de temps de bannissements*/
+setInterval(() => {
+    var sql = "DELETE FROM ban WHERE temps < NOW() + INTERVAL 1 HOUR";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table ban rafraichi");
+    });
+}, 60000);
 
 
 
@@ -48,17 +61,6 @@ io.on('connection', (socket) => {
     });
 });
 
-function create_token() {
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < 10; i++) {
-        result += characters.charAt(Math.floor(Math.random() *
-            charactersLength));
-    }
-    return result;
-}
-
 
 
 //lorsque qu'on est connecté
@@ -74,7 +76,6 @@ io.on('connection', (socket) => {
 
     socket.on('connection-user-no-pseudo', (pseudo) => {
         var valid = true;
-        var myrandomtoken = create_token();
         for (ids in connected_users) {
             if ((connected_users[ids][1] == pseudo)) {
                 valid = false;
@@ -92,7 +93,7 @@ io.on('connection', (socket) => {
         function second() {
             if (valid) {
                 socket.emit('connection_no_pseudo_valid');
-                connected_users[socket.id] = ['empty', pseudo, myrandomtoken];
+                connected_users[socket.id] = ['empty', pseudo];
             }
             else socket.emit("connection_no_pseudo_invalid");
         }
@@ -103,7 +104,6 @@ io.on('connection', (socket) => {
 
     socket.on('connection-user', (pseudo, mdp) => {
         var valid = true;
-        var myrandomtoken = create_token();
 
         for (ids in connected_users) {
             if ((connected_users[ids][1] == pseudo)) {
@@ -123,7 +123,7 @@ io.on('connection', (socket) => {
         function second() {
             if (valid) {
                 socket.emit("connection_valid");
-                connected_users[socket.id] = ['empty', pseudo, myrandomtoken];
+                connected_users[socket.id] = ['empty', pseudo];
             }
             else socket.emit("connection_invalid");
 
@@ -136,7 +136,6 @@ io.on('connection', (socket) => {
 
     socket.on('inscription-user', (pseudo, mdp, email) => {
         var valid = true;
-        var myrandomtoken = create_token();
 
         for (ids in connected_users) {
             if ((connected_users[ids][1] == pseudo)) {
@@ -169,7 +168,7 @@ io.on('connection', (socket) => {
                     console.log("user enregistré dans la bdd");
                 });
                 socket.emit('inscription_valid');
-                connected_users[socket.id] = ['empty', pseudo, myrandomtoken];
+                connected_users[socket.id] = ['empty', pseudo];
             }
         }
         if (valid) first();
@@ -182,6 +181,13 @@ io.on('connection', (socket) => {
         for (ids in connected_users) {
             if (connected_users[ids][1] == pseudo) {
                 connected_users[ids][0] = socket.id;
+
+                /* Met l'utilisateur dans la table testonline qui permet de référencer les personnes en ligne */
+                var sql = "INSERT INTO testonline (id, pseudo) VALUES ('" + socket.id + "', '" + pseudo + "')";
+                con.query(sql, function (err, result) {
+                    if (err) throw err;
+                    console.log("1 utilisateur en ligne de test online");
+                });
             }
         }
         io.emit('user-connected', connected_users);
@@ -201,6 +207,13 @@ io.on('connection', (socket) => {
         if (mytoken in connected_users) {
             if (connected_users[mytoken][0] != 'empty') {
                 io.emit('user-disconnected', connected_users[mytoken][1]);
+
+                var sql = "DELETE FROM testonline WHERE id = '" + socket.id + "'";
+                con.query(sql, function (err, result) {
+                    if (err) throw err;
+                    console.log("1 utilisateur deconnecte de testonline");
+                })
+
                 delete connected_users[mytoken];
             }
         }
@@ -208,18 +221,140 @@ io.on('connection', (socket) => {
     })
 
 
+    /* Deconnecte celui qui envoie le signal */
+    socket.on('forceDisconnect', function () {
+        socket.disconnect();
+    });
 
+    /* Envoie un signal à "nom" qui permet de le bannir, si c'est un utilisateur, il sera mis dans la table ban sinon il créera un cookie ban, puis il se fait deconnecter du serveur */
+    socket.on("ban", (duree, nom) => {
+
+        var sql = "SELECT id FROM testonline WHERE pseudo = '" + nom + "';SELECT * FROM utilisateurs where pseudo = '" + nom + "'";
+        con.query(sql, function (err, results) {
+            if (err) throw err;
+
+            if (results[0][0]) {
+                if (results[1][0]) {
+                    let temps = "";
+                    if (duree)
+                        temps = "+" + duree;
+                    var sql = "insert into ban (userID,temps) Values('" + results[1][0].id + "',NOW()+ interval + 60" + temps + " minute);"
+                    con.query(sql, function (err) {
+                        if (err) throw err;
+                    })
+                }
+                else {
+                    if (duree) {
+
+                        io.to(results[0][0].id).emit("cookie", duree);
+                    }
+                    else {
+                        io.to(results[0][0].id).emit("cookie", 0);
+                    }
+                }
+
+
+                io.to(results[0][0].id).emit("deco", "ban");
+                console.log("Utilisateur banni");
+            }
+            
+        })
+    })
+
+    /* Verifie si l'utilisateur est dans la table ban */
+    socket.on("ban?", (pseudo) => {
+        con.query("SELECT id FROM utilisateurs WHERE pseudo ='" + pseudo + "'", function (err, result) {
+            con.query("SELECT userID FROM ban WHERE userID = '" + result[0].id + "'", function (err, res) {
+                socket.emit("ban?", res[0]);
+            });
+
+        });
+    })
+
+    /* Envoie un signal à "nom" qui permet de le deconnecter */
+    socket.on("kick", (nom) => {
+        var sql = "SELECT id FROM testonline WHERE pseudo = '" + nom + "'";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            if (result[0])
+                io.to(result[0].id).emit("deco", "kick");
+            console.log("Utilisateur expulse");
+        })
+    })
+
+    socket.on("admin?", (username) => {
+        con.query("Select admin from utilisateurs where pseudo = '" + username + "'", function (err, result) {
+            if (err) throw err;
+            if (result[0]) {
+                if (result[0].admin == 1) {
+                    socket.emit("admin", true);
+                }
+            }
+        })
+    })
+
+    socket.on("mettreadmin", (username) => {
+        con.query("Select admin from utilisateurs where pseudo = '" + username + "'", function (err, result) {
+            if (err) throw err;
+            if (result[0]) {
+                con.query("UPDATE utilisateurs SET admin = 1 WHERE pseudo = '" + username + "'", function (err, result) {
+                    if (err) throw err;
+                })
+            }
+        })
+    })
+
+    socket.on("enlever", (username) => {
+        con.query("Select admin from utilisateurs where pseudo = '" + username + "'", function (err, result) {
+            if (err) throw err;
+            if (result[0]) {
+                con.query("UPDATE utilisateurs SET admin = 0 WHERE pseudo = '" + username + "'", function (err, result) {
+                    if (err) throw err;
+                })
+            }
+        })
+    })
 
 });
 
 
 
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' });
-
-
 io.on('connection', (socket) => {
+
+
     socket.on('chat message', (msg) => {
         msg["msg"] = emoji.emojify(msg["msg"]);
-        io.emit('chat message', (msg));
+
+        if (msg["msg"][0] == "/") {
+            if (msg["msg"].slice(0, 3) == "/w ")
+                whisper(msg);
+            
+        }
+        else
+            io.emit('chat message', (msg));
+
     });
+    
+      function whisper(msg) {
+
+        let pm_user = "";
+        let i = 3;
+        while (msg["msg"][i] != " ") {
+            i++;
+        }
+        pm_user = msg["msg"].slice(3, i);
+        msg["msg"] = msg["msg"].slice(i);
+
+        var sql = "SELECT id FROM testonline WHERE pseudo = '" + pm_user + "'";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            if (result[0]) {
+                io.to(socket.id).emit('private message', (msg));
+                io.to(result[0].id).emit('private message', (msg));
+            }
+
+        })
+
+    }
+    
 });
